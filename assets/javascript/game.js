a98b542ca5c6f3a267a91c8acca75a3c14f10a6a
Tom Elliott <xenomus@gmail.com>
2019-08-10 22:06:15 -0500

    most features implemented

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,17 +8,21 @@ var dummyWord = [];
 var emptyWord = [];
 var guessesMade = [];
 var remainingGuesses;
-// var alphabet = ["a", "b", "c", "d", "e", ]
 
 
-// Randomly choose word to guess from array
+// Initialize variables, display on page properly
 
 function initialize() {
+    dummyWord.length = 0;
+    emptyWord.length = 0;
+    guessesMade.length = 0;
+
+    // Randomly choose word to guess from array
+
     currentWord = words[Math.floor(Math.random() * 9)];
     remainingGuesses = 15;
     const a = document.getElementById("guessesremain");
     a.innerHTML = 15 + "<br>";
-    guessesMade.length = 0;
     for (var i = 0; i < currentWord.length; i++) {
         dummyWord[i] = currentWord.charAt(i).toLowerCase();
     }
@@ -33,30 +37,60 @@ function initialize() {
 
 function displayBlank() {
     for (var i=0; i < currentWord.length; i++) {
-        emptyWord.push("-");
+        emptyWord.push("_");
     }
     const z = document.getElementById("currentword");
     z.innerHTML = emptyWord.join(" ");
     console.log(emptyWord);
 };
 
+
+initialize();
+
 // Listen for key presses and store
 
 document.onkeyup = function() {
     keyPress = event.key;
-    console.log(keyPress);
+
+    // If a letter matches any letter in selected word, display it
+    if (dummyWord.includes(keyPress)) {
+        for (var i=0; i < dummyWord.length; i++) {
+            if ((keyPress===dummyWord[i])&&(keyPress!=emptyWord[i])) {
+                emptyWord[i] = dummyWord[i];
+                const x = document.getElementById("currentword");
+                x.innerHTML = emptyWord.join(" ");
+            }
+        }
+        console.log("empty " + emptyWord.join() + "dummy " + dummyWord.join());
+        if (emptyWord.join()==dummyWord.join()) {
+            winCount++;
+            const x = document.getElementById("gamewins");
+            x.innerHTML = winCount + "<br>";
+            const y = document.getElementById("answer");
+            y.innerHTML = currentWord + "<br><br>";
+            const z = document.getElementById("image");
+            z.
+            initialize();
+        }
+    }
+
+    // If wrong letter chosen, letter guess counter goes down and wrong guesses are displayed
+
+    else {
+        if (remainingGuesses!=0) {
+            guessesMade.push(keyPress);
+            const g = document.getElementById("lettersguessed");
+            g.innerHTML = guessesMade.join(" ");
+            remainingGuesses = remainingGuesses - 1;
+            const x = document.getElementById("guessesremain");
+            x.innerHTML = remainingGuesses + "<br>";
+        }
+        else {
+            initialize();
+            return;
+        }
+    }
 };
-
-// If not a valid letter press, ignore
-
-// If a letter matches any letter in selected word, display it
-
-// for (var i = 0; )
-
-
-
-
-// If wrong letter chosen, letter guess counter goes down and wrong guess are displayed
 
 // If word is correctly guessed, display word, image and play music; update win counter
 
