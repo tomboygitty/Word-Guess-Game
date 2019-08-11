693732dc2eb69e7e78e6624c6c6941f038a711b4
Tom Elliott <xenomus@gmail.com>
2019-08-10 22:40:14 -0500

    complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,10 @@ var emptyWord = [];
 var guessesMade = [];
 var remainingGuesses;
 
+// Initialize audio element for win
+
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "assets/twinpeaks.mp3");
 
 // Initialize variables, display on page properly
 
@@ -44,6 +48,37 @@ function displayBlank() {
     console.log(emptyWord);
 };
 
+function setImage() {
+    const z = document.getElementById("image");
+    if (currentWord==="redroom") {
+        z.src = "assets/images/redroom.jpg";
+    }
+    else if (currentWord==="laurapalmer") {
+        z.src = "assets/images/laura.jpg";
+    }
+    else if (currentWord==="dalecooper") {
+        z.src = "assets/images/dale.jpg";
+    }
+    else if (currentWord==="audreyhorne") {
+        z.src = "assets/images/audrey.jpg";
+    }
+    else if (currentWord==="BOB") {
+        z.src = "assets/images/bob.jpg";
+    }
+    else if (currentWord==="MIKE") {
+        z.src = "assets/images/mike.jpg";
+    }
+    else if (currentWord==="sherifftruman") {
+        z.src = "assets/images/sheriff.jpg";
+    }
+    else if (currentWord==="themanfromanotherplace") {
+        z.src = "assets/images/another.jpg";
+    }
+    else if (currentWord==="theloglady") {
+        z.src = "assets/images/log.jpg";
+    }
+};
+
 
 initialize();
 
@@ -51,25 +86,29 @@ initialize();
 
 document.onkeyup = function() {
     keyPress = event.key;
+    audioElement.pause();
 
     // If a letter matches any letter in selected word, display it
     if (dummyWord.includes(keyPress)) {
         for (var i=0; i < dummyWord.length; i++) {
-            if ((keyPress===dummyWord[i])&&(keyPress!=emptyWord[i])) {
+            if (keyPress===dummyWord[i]) {
                 emptyWord[i] = dummyWord[i];
                 const x = document.getElementById("currentword");
                 x.innerHTML = emptyWord.join(" ");
             }
         }
         console.log("empty " + emptyWord.join() + "dummy " + dummyWord.join());
+
+        // If word is correctly guessed, display word, image and play music; update win counter
+
         if (emptyWord.join()==dummyWord.join()) {
             winCount++;
             const x = document.getElementById("gamewins");
             x.innerHTML = winCount + "<br>";
             const y = document.getElementById("answer");
             y.innerHTML = currentWord + "<br><br>";
-            const z = document.getElementById("image");
-            z.
+            setImage();
+            audioElement.play();
             initialize();
         }
     }
@@ -77,24 +116,22 @@ document.onkeyup = function() {
     // If wrong letter chosen, letter guess counter goes down and wrong guesses are displayed
 
     else {
-        if (remainingGuesses!=0) {
-            guessesMade.push(keyPress);
-            const g = document.getElementById("lettersguessed");
-            g.innerHTML = guessesMade.join(" ");
-            remainingGuesses = remainingGuesses - 1;
-            const x = document.getElementById("guessesremain");
-            x.innerHTML = remainingGuesses + "<br>";
-        }
-        else {
-            initialize();
-            return;
+        if (!guessesMade.join().includes(keyPress)) {
+            if (remainingGuesses!=0) {
+                guessesMade.push(keyPress);
+                const g = document.getElementById("lettersguessed");
+                g.innerHTML = guessesMade.join(" ");
+                remainingGuesses = remainingGuesses - 1;
+                const x = document.getElementById("guessesremain");
+                x.innerHTML = remainingGuesses + "<br>";
+            }
+            else {
+                initialize();
+                return;
+            }
         }
     }
 };
-
-// If word is correctly guessed, display word, image and play music; update win counter
-
-// If word is not guessed, play fail sound
 
 // On win or lose, load next word to guess automatically
 
